f095464f0199bc5f2bb20b8049f7c25bba76311a
Ugyen Norbu <66914661+UgyenNorbu@users.noreply.github.com>
2022-06-13 00:14:26 +0600

    JS update
    Random code generation in JS incorporated

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const colors = ["green", "red"];
+var characters = 'abcdef0123456789'
+var val = ""
+var outCode = ""
+
+function genCode(lenCode) {
+    for(var i = 0; i < lenCode; i++){
+        let count = Math.floor(Math.random()*16)
+        val = characters.charAt(count)
+        //console.log(count)
+        //console.log(val)
+        outCode = outCode + val
+    }
+    return outCode
+}
+
+console.log(genCode(6))
+
+// Listen for Click
+// Generate Hexcode
+// Assign the Hexcode to the background color of the body div
