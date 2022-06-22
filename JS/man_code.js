bda10a53125261e2207eeb64a528f0b522525695
Ugyen Norbu <66914661+UgyenNorbu@users.noreply.github.com>
2022-06-22 10:29:43 +0600

    Hash  text included

--- JS/man_code.js
+++ JS/man_code.js
@@ -1,16 +1,30 @@
 const body = document.querySelector(".body");
+var element = document.getElementById('user-input');
+
+element.addEventListener('focus', function() {
+  element.setAttribute('placeholder', '')
+
+})
+
+element.addEventListener('blur', function() {
+  element.setAttribute('placeholder', 'Enter hex code here!!!')
+
+})
 
 document.getElementById('submit-btn').onclick = function () {
     var userInput = document.getElementById('user-input').value;
     let strLen = userInput.length
-    console.log(strLen);
 
     if(strLen != 6) {
         alert("Enter a valid hex code!");
     }
     else{
         for(let i = 0; i < 6; i++){
-            '0123456789abcdef'.includes(userInput[i]);
+            if(!'0123456789abcdef'.includes(userInput[i])){
+                alert("Enter a valid hex code!");
+            }
         }
+        body.style.backgroundColor = '#' + userInput;
     }
 }
+
